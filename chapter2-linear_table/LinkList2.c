766347e9cc31196d85bfd597995f88e4b0497787
gaoziqiang <gaoziqianghi@163.com>
2017-10-25 17:15:08 +0800

    添加遍历

--- chapter2-linear_table/LinkList2.c
+++ chapter2-linear_table/LinkList2.c
@@ -20,30 +20,52 @@ typedef int ElemType;
 
 typedef struct LNode {
     ElemType data; //数据域
+    int length;
     struct LNode *next; //指针域
 } LNode, *LinkList;
 
-
-void CreateLinkList(LNode *L,int n) {
+void CreateLinkList(LNode *L, int n) {
 
     int i;
-    
+
     L = (LinkList) malloc(sizeof (LNode));
     L->next = NULL;
+    printf("***链表创建中***\n");
     for (i = n; i > 0; --i) {
 
-        printf("链表创建中:%d\n",i);
+        printf("请输入第 %d 个元素:\n", 6 - i);
         LNode *p;
-        p = (LinkList) malloc(sizeof (LNode));// 创建节点
-        printf("链表创建成功\n");
-        //printf("请输入第 " + i + "个元素:\n");
-        printf("链表创建成功\n");
-        scanf("%d",&p->data);
+        p = (LinkList) malloc(sizeof (LNode)); // 创建节点
+        
+        scanf("%d", &p->data);
+        ++L->length;
         p->next = L->next;
         L->next = p;
+        printf("打印元素%d:%d \n",6 - i,p->data);
     }
+    printf("length:%d\n",L->length);
+    printf("***链表创建完毕***\n");
+}
+
+void TraverseList(LNode *L) {
     
-    printf("链表创建成功\n");
+    LNode *p;
+    p = L->next;
+    printf("打印L->next%p\n",L->next);
+    printf("打印p%p\n",p);
+    printf("打印p.data%d\n",p->data);
+    //p = L->next;
+    printf("***开始遍历***\n");
+    while(p != NULL) {
+        printf("test1");
+        printf("%d\n",p->data);
+        printf("test2");
+        p = p->next;
+        printf("test3");
+    } 
+    
+    printf("***遍历结束***\n");
+        
 }
 /*
  * 
@@ -53,11 +75,12 @@ int main() {
     printf("Hello World!\n");
 
     LNode L;
-    
+
     int n = 5;
-    
+
     CreateLinkList(&L,5);
-    
+    //TraverseList(&L);
+
     return 0;
 }
 
